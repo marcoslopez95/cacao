096d95627c4af4d522279f638720b86bd6ac3d5d
Marcos López <marcoslpez@pop-os.Home>
2026-04-23 15:25:41 -0400

    feat: replace Laravel SVG logo with CACAO 3x3 isotipo

--- resources/js/components/AppLogoIcon.vue
+++ resources/js/components/AppLogoIcon.vue
@@ -3,7 +3,7 @@ defineOptions({ inheritAttrs: false });
 </script>
 
 <template>
-    <div class="grid grid-cols-3 gap-0.75 w-6" v-bind="$attrs">
+    <div class="grid grid-cols-3 gap-0.75 w-6" role="img" aria-label="CACAO" v-bind="$attrs">
         <span class="rounded-xs aspect-square bg-foreground"></span>
         <span class="rounded-xs aspect-square bg-foreground"></span>
         <span class="rounded-xs aspect-square bg-terracota dark:bg-terra-hover"></span>
