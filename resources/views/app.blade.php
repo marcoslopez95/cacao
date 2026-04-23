f7caf7bd4712af2fa4dae5074c2c17600fc825e4
Marcos López <marcoslpez@pop-os.Home>
2026-04-23 14:46:14 -0400

    chore: remove Instrument Sans, apply CACAO inline bg colors

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -19,23 +19,15 @@
             })();
         </script>
 
-        {{-- Inline style to set the HTML background color based on our theme in app.css --}}
+        {{-- Inline style to set the HTML background color before CSS loads --}}
         <style>
-            html {
-                background-color: oklch(1 0 0);
-            }
-
-            html.dark {
-                background-color: oklch(0.145 0 0);
-            }
+            html { background-color: #EDEBE7; }
+            html.dark { background-color: #131110; }
         </style>
 
         <link rel="icon" href="/favicon.ico" sizes="any">
         <link rel="icon" href="/favicon.svg" type="image/svg+xml">
         <link rel="apple-touch-icon" href="/apple-touch-icon.png">
-
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
 
         @vite(['resources/css/app.css', 'resources/js/app.ts', "resources/js/pages/{$page['component']}.vue"])
         <x-inertia::head>
