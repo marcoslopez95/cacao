7ccf6b461e0ff7acb1ccfd21dbe723d2c3ba7b81
Marcos López <marcoslpez@pop-os.Home>
2026-05-21 20:01:55 -0400

    fix: extend Controller in portal dashboard controllers

--- app/Http/Controllers/Student/DashboardController.php
+++ app/Http/Controllers/Student/DashboardController.php
@@ -2,11 +2,12 @@
 
 namespace App\Http\Controllers\Student;
 
+use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
 use Inertia\Response;
 
-class DashboardController
+class DashboardController extends Controller
 {
     public function index(Request $request): Response
     {
